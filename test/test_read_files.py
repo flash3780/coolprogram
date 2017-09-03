6e28b6bba3deedd0e2f4e1687b6c28c1626110ea
Chris Hubley <chris.hubley@gmail.com>
2017-09-03 14:46:13 -0400

    Added try/except to try both locations for test files

--- test/test_read_files.py
+++ test/test_read_files.py
@@ -7,6 +7,13 @@ def test_read_favorite_color():
     Test that favorite color is read properly
 
     """
-    data = read_favorite_color('./test/test_files/favorite_color.csv')
+    try:
+        data = read_favorite_color('./test_files/favorite_color.csv')
+    except IOError:
+        try:
+            data = read_favorite_color('./test/test_files/favorite_color.csv')
+        except IOError:
+            print('Cannot find test file')
+
     assert( data['correct_answers'][0] == 0 )
     assert( data['cross_bridge'][0] == False )
