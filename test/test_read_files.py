dba16aea053a0ddbd7e7c98ba4728cbe53fb2fc1
Chris Hubley <chris.hubley@gmail.com>
2017-09-03 15:33:37 -0400

    Fixed error checking for test file.

--- test/test_read_files.py
+++ test/test_read_files.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 
 from coolprogram import *
+import os
 
+def find_file(locations):
+    for location in locations:
+        if os.path.exists(location):
+            return(location)
+    raise IOError('Could not find test file.')
+        
 def test_read_favorite_color():
     """
     Test that favorite color is read properly
 
     """
-    try:
-        data = read_favorite_color('./test_files/favorite_color.csv')
-    except IOError:
-        try:
-            data = read_favorite_color('./test/test_files/favorite_color.csv')
-        except IOError:
-            print('Cannot find test file')
+    locations = ['./test_files/favorite_color.csv',
+                 './test/test_files/favorite_color.csv',
+                ]
+    test_file = find_file(locations)
 
+    data = read_favorite_color(test_file)
     assert( data['correct_answers'][0] == 0 )
     assert( data['cross_bridge'][0] == False )
